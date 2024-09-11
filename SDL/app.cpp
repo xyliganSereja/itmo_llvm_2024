a959e64096af97834e81282cd4a7c6fe80987781
_lil_ya_da_da_ <113226520+xyliganSereja@users.noreply.github.com>
2024-09-11 22:56:03 +0300

    Update logic

--- SDL/app.cpp
+++ SDL/app.cpp
@@ -1,13 +1,9 @@
 #include "sim.h"
-#include <SDL2/SDL.h>
 
 #define ALIVE 0xFF00FF00
 #define DEAD 0xFF000000
 
-static int world[SIM_Y_SIZE][SIM_X_SIZE];
-static int new_world[SIM_Y_SIZE][SIM_X_SIZE];
-
-void init_game() {
+void init_game(int (*world)[SIM_X_SIZE], int (*new_world)[SIM_X_SIZE]) {
   for (int y = 0; y < SIM_Y_SIZE; ++y) {
     for (int x = 0; x < SIM_X_SIZE; ++x) {
       world[y][x] = simRand() % 2;
@@ -15,7 +11,7 @@ void init_game() {
   }
 }
 
-void draw() {
+void draw(int (*world)[SIM_X_SIZE], int (*new_world)[SIM_X_SIZE]) {
   for (int y = 0; y < SIM_Y_SIZE; ++y) {
     for (int x = 0; x < SIM_X_SIZE; ++x) {
       int color = world[y][x] ? ALIVE : DEAD;
@@ -26,23 +22,27 @@ void draw() {
   simFlush();
 }
 
-int get_state(int x, int y) {
-  if (x > 0 && y > 0 && x < SIM_X_SIZE && y < SIM_Y_SIZE) {
-    return world[x][y];
+int get_state(int x, int y, int (*world)[SIM_X_SIZE],
+              int (*new_world)[SIM_X_SIZE]) {
+  if (x >= 0 && y >= 0 && x < SIM_X_SIZE && y < SIM_Y_SIZE) {
+    return world[y][x];
   }
 
   return 0;
 }
 
-int get_count_neib(int x, int y) {
-  return get_state(x, y - 1) + get_state(x - 1, y) + get_state(x, y + 1) +
-         get_state(x + 1, y);
+int get_count_neib(int x, int y, int (*world)[SIM_X_SIZE],
+                   int (*new_world)[SIM_X_SIZE]) {
+  return get_state(x, y - 1, world, new_world) +
+         get_state(x - 1, y, world, new_world) +
+         get_state(x, y + 1, world, new_world) +
+         get_state(x + 1, y, world, new_world);
 }
 
-void update() {
+void update(int (*world)[SIM_X_SIZE], int (*new_world)[SIM_X_SIZE]) {
   for (int y = 0; y < SIM_Y_SIZE; ++y) {
     for (int x = 0; x < SIM_X_SIZE; ++x) {
-      int count = get_count_neib(x, y);
+      int count = get_count_neib(x, y, world, new_world);
       if (world[y][x] == 1) {
         if (count == 2 || count == 3) {
           new_world[y][x] = 1;
@@ -67,10 +67,17 @@ void update() {
 }
 
 void app() {
-  init_game();
+  int world[SIM_Y_SIZE][SIM_X_SIZE];
+  int new_world[SIM_Y_SIZE][SIM_X_SIZE];
+  for (int y = 0; y < SIM_Y_SIZE; ++y) {
+    for (int x = 0; x < SIM_X_SIZE; ++x) {
+      world[y][x] = 0;
+      new_world[y][x] = 0;
+    }
+  }
+  init_game(world, new_world);
   while (true) {
-    draw();
-    update();
-    SDL_Delay(100);
+    draw(world, new_world);
+    update(world, new_world);
   }
 }
